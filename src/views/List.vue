5e4c2938c4bd7b79544d3ce218ef02001125dbc9
haochengz <haochengzhao@outlook.com>
2018-05-28 21:03:46 +0800

    add price filter function

--- src/views/List.vue
+++ src/views/List.vue
@@ -10,16 +10,16 @@
       <span class="sortby">Sort by:</span>
       <a href="javascript:void(0)" class="default cur">Default</a>
       <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
-      <a href="javascript:void(0)" class="filterby stopPop">Filter by</a>
+      <a href="javascript:void(0)" class="filterby stopPop" @click="popFilter">Filter by</a>
     </div>
     <div class="accessory-result">
       <!-- filter -->
-      <div class="filter stopPop" id="filter">
+      <div class="filter stopPop" v-bind:class="{'filterby-show':filterPop}" id="filter">
         <dl class="filter-price">
           <dt>Price:</dt>
-          <dd><a href="javascript:void(0)">All</a></dd>
-          <dd>
-            <a href="javascript:void(0)">0 - 100</a>
+          <dd><a href="javascript:void(0)" v-bind:class="{'cur': priceChecked == 'all'}" @click="priceFilterReset">All</a></dd>
+          <dd v-for="(price, index) in priceFilter" v-bind:key="index">
+            <a href="javascript:void(0)" v-bind:class="{'cur': priceChecked == index}" @click="priceFilterClick(index)">{{price.start}} - {{price.end}}</a>
           </dd>
         </dl>
       </div>
@@ -47,6 +47,7 @@
   </div>
 </div>
 
+<div class="md-overlay" v-show="overLayFlag" @click="closeFilter"></div>
 <nav-footer></nav-footer>
 </div>
 </template>
@@ -72,7 +73,24 @@ export default {
     },
     data() {
         return {
-            goods: []
+            goods: [],
+            priceFilter: [
+              {
+                start: '0.00',
+                end: '500.00'
+              },
+              {
+                start: '500.00',
+                end: '1000.00'
+              },
+              {
+                start: '1000.00',
+                end: '2000.00'
+              },
+            ],
+            priceChecked: "all",
+            filterPop: false,
+            overLayFlag: false
         }
     },
     methods: {
@@ -87,6 +105,22 @@ export default {
             }).catch(err => {
                 console.log("Something wrong when asking goods data")
             })
+        },
+        priceFilterClick(index) {
+          this.priceChecked = index
+          this.closeFilter()
+        },
+        priceFilterReset() {
+          this.priceChecked = 'all'
+          this.closeFilter()
+        },
+        popFilter() {
+          this.filterPop = true
+          this.overLayFlag = true
+        },
+        closeFilter() {
+          this.filterPop = false
+          this.overLayFlag = false
         }
     },
     mounted() {
