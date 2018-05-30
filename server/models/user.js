44f44c3eb6cdf42c2cebfbcf709d6e65c7cdf99e
haochengz <haochengzhao@outlook.com>
2018-05-30 21:45:27 +0800

    add user model

---
+++ server/models/user.js
@@ -0,0 +1,21 @@
+
+const mongoose = require('mongoose')
+
+module.exports = mongoose.model('users', new mongoose.Schema({
+    "id": String,
+    "username": String,
+    "email": String,
+    "password": String,
+    "itemsInCart": [
+        {
+            "productId": String,
+            "productName": String,
+            "productPrice": Number,
+            "productImg": String,
+            "isSelected": Boolean,
+            "numbers": Number,
+        }
+    ],
+    "orders": Array,
+    "address": Array
+}))
