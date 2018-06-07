4277f801054b8b6ec7122d8ce21c0f0322c5145c
haochengz <haochengzhao@outlook.com>
2018-06-08 00:24:20 +0800

    set up a new page: cart page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import List from '@/views/List'
+import Cart from '@/views/Cart'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'List',
       component: List
+    },
+    {
+      path: '/cart',
+      name: 'Cart',
+      component: Cart
     }
   ]
 })
---
+++ src/views/Cart.vue
@@ -0,0 +1,30 @@
+
+<template>
+    <div>
+        <nav-header></nav-header>
+        <nav-crumbs><span>Counter</span></nav-crumbs>
+        <nav-footer></nav-footer>
+    </div>
+</template>
+
+<script>
+import './../assets/css/base.css'
+import './../assets/css/product.css'
+import './../assets/css/checkout.css'
+import './../assets/css/login.css'
+
+import NavHeader from '../components/navheader'
+import NavCrumbs from '../components/navcrumbs'
+import NavFooter from '../components/navfooter'
+import Modal from '../components/modal'
+// import axios from 'axios'
+
+export default {
+  components: {
+    NavHeader,
+    NavCrumbs,
+    NavFooter,
+    Modal
+  }
+}
+</script>
